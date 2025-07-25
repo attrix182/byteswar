105427c96890743f16b69c0faa3b82b7124f4d0c
Luciano S <lucho.bernal@hotmail.com>
2025-07-25 19:06:14 -0300

    player mas grande

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -14,7 +14,7 @@ export const Robot: React.FC<RobotProps> = ({ player, isLocal }) => {
     mass: 1,
     position: player.position,
     rotation: player.rotation,
-    args: [1, 0.5, 1.5], // Ancho, alto, largo
+    args: [1.5, 0.75, 2.25], // Ancho, alto, largo - Aumentado 50%
     material: { friction: 0.7 }
   }))
 
@@ -45,60 +45,60 @@ export const Robot: React.FC<RobotProps> = ({ player, isLocal }) => {
   return (
     <group ref={ref as any}>
       {/* Cuerpo principal del robot */}
-      <mesh position={[0, 0.25, 0]}>
-        <cylinderGeometry args={[0.6, 0.6, 0.5, 8]} />
+      <mesh position={[0, 0.375, 0]}>
+        <cylinderGeometry args={[0.9, 0.9, 0.75, 8]} />
         <meshStandardMaterial color={player.color} />
       </mesh>
 
       {/* Cabeza del robot */}
-      <mesh position={[0, 0.75, 0]}>
-        <sphereGeometry args={[0.3, 8, 6]} />
+      <mesh position={[0, 1.125, 0]}>
+        <sphereGeometry args={[0.45, 8, 6]} />
         <meshStandardMaterial color={player.color} />
       </mesh>
 
       {/* Cañón */}
-      <mesh position={[0, 0.5, 0.8]} rotation={[0, 0, 0]}>
-        <cylinderGeometry args={[0.05, 0.05, 0.4, 8]} />
+      <mesh position={[0, 0.75, 1.2]} rotation={[0, 0, 0]}>
+        <cylinderGeometry args={[0.075, 0.075, 0.6, 8]} />
         <meshStandardMaterial color="#333" />
       </mesh>
 
       {/* Ruedas */}
-      <group ref={(el) => el && (wheelRefs.current[0] = el)} position={[-0.4, -0.1, -0.3]}>
+      <group ref={(el) => el && (wheelRefs.current[0] = el)} position={[-0.6, -0.15, -0.45]}>
         <mesh rotation={[0, 0, Math.PI / 2]}>
-          <cylinderGeometry args={[0.15, 0.15, 0.05, 8]} />
+          <cylinderGeometry args={[0.225, 0.225, 0.075, 8]} />
           <meshStandardMaterial color="#222" />
         </mesh>
       </group>
 
-      <group ref={(el) => el && (wheelRefs.current[1] = el)} position={[0.4, -0.1, -0.3]}>
+      <group ref={(el) => el && (wheelRefs.current[1] = el)} position={[0.6, -0.15, -0.45]}>
         <mesh rotation={[0, 0, Math.PI / 2]}>
-          <cylinderGeometry args={[0.15, 0.15, 0.05, 8]} />
+          <cylinderGeometry args={[0.225, 0.225, 0.075, 8]} />
           <meshStandardMaterial color="#222" />
         </mesh>
       </group>
 
-      <group ref={(el) => el && (wheelRefs.current[2] = el)} position={[-0.4, -0.1, 0.3]}>
+      <group ref={(el) => el && (wheelRefs.current[2] = el)} position={[-0.6, -0.15, 0.45]}>
         <mesh rotation={[0, 0, Math.PI / 2]}>
-          <cylinderGeometry args={[0.15, 0.15, 0.05, 8]} />
+          <cylinderGeometry args={[0.225, 0.225, 0.075, 8]} />
           <meshStandardMaterial color="#222" />
         </mesh>
       </group>
 
-      <group ref={(el) => el && (wheelRefs.current[3] = el)} position={[0.4, -0.1, 0.3]}>
+      <group ref={(el) => el && (wheelRefs.current[3] = el)} position={[0.6, -0.15, 0.45]}>
         <mesh rotation={[0, 0, Math.PI / 2]}>
-          <cylinderGeometry args={[0.15, 0.15, 0.05, 8]} />
+          <cylinderGeometry args={[0.225, 0.225, 0.075, 8]} />
           <meshStandardMaterial color="#222" />
         </mesh>
       </group>
 
       {/* Indicador de salud */}
-      <mesh position={[0, 1.2, 0]}>
-        <boxGeometry args={[0.8, 0.1, 0.1]} />
+      <mesh position={[0, 1.8, 0]}>
+        <boxGeometry args={[1.2, 0.15, 0.15]} />
         <meshStandardMaterial color="#333" />
       </mesh>
       
-      <mesh position={[0, 1.2, 0]} scale={[player.health / player.maxHealth, 1, 1]}>
-        <boxGeometry args={[0.8, 0.1, 0.1]} />
+      <mesh position={[0, 1.8, 0]} scale={[player.health / player.maxHealth, 1, 1]}>
+        <boxGeometry args={[1.2, 0.15, 0.15]} />
         <meshStandardMaterial color={player.health > 50 ? "#44ff44" : player.health > 25 ? "#ffff44" : "#ff4444"} />
       </mesh>
     </group>
